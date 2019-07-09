0f48292bae097673815f9d517cdea5e291aa2712
Dusan Stevanovic <duledu24@gmail.com>
2019-07-09 12:16:45 +0200

    footer without style/js refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
 
+$('#test').on('click', function () {
+  $('#test').toggleClass('collapsed');
+  $('#collapsedegrees').toggleClass('show');
+
+});
+
+$('#test1').on('click', function () {
+  $('#test1').toggleClass('collapsed');
+  $('#collapseAbout').toggleClass('show');
+
+});
+
+$('#test2').on('click', function () {
+  $('#test2').toggleClass('collapsed');
+  $('#collapseChingu').toggleClass('show');
+
+});
+
+$('#test3').on('click', function () {
+  $('#test3').toggleClass('collapsed');
+  $('#collapseInfo').toggleClass('show');
+
+});
