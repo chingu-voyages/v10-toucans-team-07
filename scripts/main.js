501797e836c8663c64f3c7fd3ffbe9336bbb26a8
jan <46709199+jartdian@users.noreply.github.com>
2019-07-21 12:07:18 +0200

    added desktop responsiveness

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
-
-
 //  Rellax.js lib
 var rellax = new Rellax(".rellax");
+
+
+// Adding Parallax to floating card element above 1200px width
+let floating_card = document.querySelector('.floating-card');
+
 
