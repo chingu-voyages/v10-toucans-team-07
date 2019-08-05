d8b4f076b4efe15119dfe4a95157ba6532a9e3ef
Psalm Diao <28274962+psdi@users.noreply.github.com>
2019-08-05 21:08:19 +0200

    Add more breakpoints + script for altering DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,104 @@
-const allTabs = $('.course-overview > li');
-const submenus = $('.tab-content > div');
+$(document).ready(function() {
+    let allTabs = $('.course-overview > li');
+    let submenus = $('.tab-content > div');
 
-// had to rewrite Bootstrap's tab function because it was inconsistent
-const activateTab = function(link) {
-    let linkId = link.attr('id');
-    $.each(submenus, function(t) {
-        if (submenus.eq(t).hasClass('active')) {
-            submenus.eq(t).removeClass('active');
-        }
-        if (linkId === submenus.eq(t).attr('aria-labelledby')) {
-            submenus.eq(t).addClass('active');
-        }
-    });
+    // Had to rewrite Bootstrap's tab function because it was inconsistent
+    const activateTab = function(link) {
+        let linkId = link.attr('id');
+        submenus = $('.tab-content > div');
 
-};
+        $.each(submenus, function(t) {
+            if (submenus.eq(t).hasClass('active')) {
+                submenus.eq(t).removeClass('active');
+            }
+            if (linkId === submenus.eq(t).attr('aria-labelledby')) {
+                submenus.eq(t).addClass('active');
+            }
+        });
 
-$('.nav-tabs a').on('click', function(e) {
-    e.stopPropagation();
-    activateTab($(this));
-    $.each(allTabs, function(t) {
-        if (allTabs.eq(t).hasClass('active')) {
-            allTabs.eq(t).removeClass('active');
+    };
+
+    let activateTabEvent = function(e) {
+        allTabs = $('.course-overview > li')
+        e.stopPropagation();
+        activateTab($(this));
+        $.each(allTabs, function(t) {
+            if (allTabs.eq(t).hasClass('active')) {
+                allTabs.eq(t).removeClass('active');
+            }
+        });
+        $(this).parent().addClass('active');
+    };
+
+    // $('.nav-tabs a').on('click', activateTabEvent);
+
+
+    // --- Dropdown events ---
+    let showDropdown = function() {
+        $(this).children('.submenu').toggleClass('visible');
+    };
+
+    let hideDropdown = function() {
+        $(this).children('.submenu').removeClass('visible');
+    };
+
+    let dropdowns = $('.dropdown');
+    dropdowns.on('click', showDropdown).on('focusout', hideDropdown);
+
+    
+    // --- Change appearance when width <= 975 ---
+    const BREAKPOINT = 975;
+
+    const headerNavbar = $('header > .fixed-top');
+    const topMiddleNav = $('#top-middle-nav');
+
+    function breakpointReached() {
+        return window.innerWidth <= BREAKPOINT;
+    }
+
+    function createNewMenu() {
+        let menu = $('<div></div>');
+        menu.attr('id', 'newMenu');
+        return menu;
+    }
+
+    function rebootEvents() {
+        $('.dropdown').on('click', showDropdown).on('focusout', hideDropdown);
+        $('.nav-tabs a').on('click', activateTabEvent);
+    }
+
+    function adjustHeader() {
+        let breakpoint = breakpointReached();
+
+        if (breakpoint) {
+            if ( $('#newMenu').length === 0 ) {
+                let newMenu = createNewMenu();
+                newMenu.append(topMiddleNav);
+                if ($('#top-middle-nav')) {
+                    topMiddleNav.remove();
+                }
+
+                $(headerNavbar).append(newMenu);
+                $(newMenu).append(topMiddleNav);
+            }
+        } else {
+            if ( $('#newMenu').length > 0 ) {
+                let menu = $('#newMenu');
+                menu.remove();
+                topMiddleNav.insertBefore($('#top-right-nav'));
+            }
         }
-    });
-    $(this).parent().addClass('active');
+
+        rebootEvents();
+    }
+
+    window.onresize = _.debounce(adjustHeader, 20);
+    adjustHeader();
+
+
+    //  Rellax.js lib
+    var rellax = new Rellax(".rellax");
+
+    // Adding Parallax to floating card element above 1200px width
+    let floating_card = document.querySelector('.floating-card');
 });
-
-const dropdowns = $('.dropdown');
-dropdowns.on('click', function() {
-    $(this).children('.submenu').toggleClass('visible');
-}).on('focusout', function() {
-    $(this).children('.submenu').removeClass('visible');
-});
-
-
-//  Rellax.js lib
-var rellax = new Rellax(".rellax");
-
-// Adding Parallax to floating card element above 1200px width
-let floating_card = document.querySelector('.floating-card');
